75267e10f576f15f9496fc6b3889ca0a6d34305e
Dolu1990 <charles.papon.90@gmail.com>
2023-11-27 14:47:48 +0100

    regression wip

--- src/test/scala/vexiiriscv/tester/Regression.scala
+++ src/test/scala/vexiiriscv/tester/Regression.scala
@@ -1,15 +1,16 @@
 package vexiiriscv.tester
 
+import org.apache.commons.io.FileUtils
 import spinal.core.sim._
+import spinal.lib.misc.test.AsyncJob
 import vexiiriscv.riscv.Riscv
+import vexiiriscv.tester.Regression.simConfig
 import vexiiriscv.{Global, ParamSimple, VexiiRiscv}
 
-import java.io.File
+import java.io.{BufferedWriter, File, FileWriter}
+import java.nio.file.{Files, Paths}
 import scala.collection.mutable.ArrayBuffer
-
-object RegressionConsts{
-
-}
+import scala.concurrent.ExecutionContext
 
 class Regression(compiled : SimCompiled[VexiiRiscv]){
   val dut = compiled.dut
@@ -23,9 +24,9 @@ class Regression(compiled : SimCompiled[VexiiRiscv]){
   val tests = ArrayBuffer[TestOptions]()
 
   //rvi tests
-  val f = new File("ext/NaxSoftware/riscv-tests")
-  val riscvTests = f.list().sorted
-  val rvi = riscvTests.filter(t => t.startsWith(s"rv${xlen}ui-p-") && !t.contains(".")).map(new File(f, _))
+  val riscvTestsFile = new File("ext/NaxSoftware/riscv-tests")
+  val riscvTests = riscvTestsFile.list().sorted
+  val rvi = riscvTests.filter(t => t.startsWith(s"rv${xlen}ui-p-") && !t.contains(".")).map(new File(riscvTestsFile, _))
   for(elf <- rvi) {
     val t = newTest()
     t.elfs += elf
@@ -35,15 +36,37 @@ class Regression(compiled : SimCompiled[VexiiRiscv]){
     tests += t
   }
 
+  implicit val ec = ExecutionContext.global
+  val jobs = ArrayBuffer[AsyncJob]()
   for(t <- tests){
-    t.test(compiled)
+    val testPath = new File(compiled.simConfig.getTestPath(t.testName.get))
+    val passFile = new File(testPath, "PASS")
+    val failFile = new File(testPath, "FAIL")
+    if(!passFile.exists()){
+      val job = new AsyncJob(toStdout = false, logsPath = testPath)({
+        t.test(compiled)
+        FileUtils.deleteQuietly(failFile)
+        val bf = new BufferedWriter(new FileWriter(passFile))
+        bf.flush()
+        bf.close()
+      }){
+        override def onFail() = {
+          val bf = new BufferedWriter(new FileWriter(failFile))
+          bf.flush()
+          bf.close()
+        }
+      }
+      jobs += job
+    }
   }
+  jobs.foreach(_.join())
 }
 
 object Regression extends App{
   val simConfig = SpinalSimConfig()
   simConfig.withFstWave
   simConfig.withTestFolder
+  simConfig.setTestPath("$WORKSPACE/$COMPILED_tests/$TEST")
 
   val param = new ParamSimple()
   val compiled = simConfig.compile(VexiiRiscv(param.plugins()))
