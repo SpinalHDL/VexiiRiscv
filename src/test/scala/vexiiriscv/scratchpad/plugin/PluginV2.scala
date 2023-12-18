7d0f97595122577d4e8638d5492f752930adb955
Dolu1990 <charles.papon.90@gmail.com>
2023-12-18 13:37:23 +0100

    sync

---
+++ src/test/scala/vexiiriscv/scratchpad/plugin/PluginV2.scala
@@ -0,0 +1,43 @@
+package vexiiriscv.scratchpad.plugin
+
+import scala.collection._
+import spinal.core._
+import spinal.core.fiber._
+import spinal.lib._
+import spinal.lib.misc.plugin._
+
+
+
+object FiberPlay extends App{
+  SpinalVerilog(new Component{
+
+    val logic = new Area {
+      var value = 0
+      val xRetainer = Retainer()
+
+      val x = Fiber setup new Area {
+        //      AsyncThread.current.setName("Xthread")
+        println("setup x")
+        Fiber.awaitBuild()
+        println("build x")
+        val miaou = Bool()
+        xRetainer.await()
+        println(value)
+        assert(value == 42)
+      }
+
+      val y = Fiber setup new Area {
+        println("setup y")
+        val xLock = xRetainer()
+        Fiber.awaitBuild()
+        println("build y")
+        value = 42
+//        xLock.releases()
+      }
+
+      val z = Fiber build new Area {
+        println("build z")
+      }
+    }
+  })
+}
