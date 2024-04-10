0ae04155e56eda299be6dfe70627aa5072835224
Dolu1990 <charles.papon.90@gmail.com>
2024-04-10 09:05:23 +0200

    openocd dev.tcl now use HART_COUNT

--- src/main/tcl/openocd/dev.tcl
+++ src/main/tcl/openocd/dev.tcl
@@ -1,6 +1,16 @@
+
+set cpu_count 1
+if [info exists env(HART_COUNT)]  {
+    set cpu_count $::env(HART_COUNT)
+}
+
+load_image /media/data2/proj/vexii/litex/buildroot/rv32ima/images_full/linux_${cpu_count}c.dtb 0x40ef0000
 load_image /media/data2/proj/vexii/litex/buildroot/rv32ima/images_full/Image 0x40000000
-load_image /media/data2/proj/vexii/litex/buildroot/rv32ima/images_full/linux_2c.dtb 0x40ef0000
 load_image /media/data2/proj/vexii/litex/buildroot/rv32ima/images_full/rootfs.cpio 0x41000000
 load_image /media/data2/proj/vexii/litex/buildroot/rv32ima/opensbi/build/platform/litex/vexriscv/firmware/fw_jump.bin 0x40f00000
-targets riscv.cpu.1; resume
-targets riscv.cpu.0; resume
+
+for {set i 0} {$i < $cpu_count} {incr i} {
+    targets $_TARGETNAME.$i
+    resume
+}
+
