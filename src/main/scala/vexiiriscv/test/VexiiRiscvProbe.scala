3d905614d0521f1a17481a65d9dbc1bccf351926
Dolu1990 <charles.papon.90@gmail.com>
2024-04-11 20:31:51 +0200

    Fix flush triggering livness assert

--- src/main/scala/vexiiriscv/test/VexiiRiscvProbe.scala
+++ src/main/scala/vexiiriscv/test/VexiiRiscvProbe.scala
@@ -532,7 +532,7 @@ class VexiiRiscvProbe(cpu : VexiiRiscv, kb : Option[konata.Backend], var withRvl
       if(((wfi >> localHartId) & 1) != 0){
         hart.lastCommitAt = cycle
       }
-      if (checkLiveness && hart.lastCommitAt + 4000l < cycle) {
+      if (checkLiveness && hart.lastCommitAt + 16000l < cycle) {
         val status = if (hart.microOpAllocPtr != hart.microOpRetirePtr) f"waiting on uop 0x${hart.microOpRetirePtr}%X" else f"last uop id 0x${hart.lastUopId}%X"
         simFailure(f"Vexii hasn't commited anything for too long, $status")
       }
