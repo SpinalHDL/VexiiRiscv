37a0eabee4a5dac3d736b153587c9933578585d2
Dolu1990 <charles.papon.90@gmail.com>
2024-05-02 13:58:13 +0200

    sync

--- src/main/scala/vexiiriscv/soc/litex/Soc.scala
+++ src/main/scala/vexiiriscv/soc/litex/Soc.scala
@@ -363,6 +363,16 @@ litex_sim --cpu-type=vexiiriscv  --with-sdram --sdram-data-width=64 --bus-standa
 
 litex_sim --cpu-type=vexiiriscv  --with-sdram --sdram-data-width=64 --bus-standard axi-lite --vexii-args="--allow-bypass-from=0 --with-mul --with-div --with-rva --with-btb --with-ras --with-gshare --fetch-l1-sets=64 --fetch-l1-ways=4 --lsu-l1-sets=64 --lsu-l1-ways=4 --lsu-l1-store-buffer-slots=2 --lsu-l1-store-buffer-ops=32 --lsu-l1-refill-count 2 --lsu-l1-writeback-count 2 --with-supervisor --with-user --performance-counters 0" --trace-fst --sdram-init /media/data2/proj/vexii/litex/buildroot/rv32ima/images/boot.json --trace-start 120000000000000 --trace-end 122000000000000 --trace
 
+litex_sim --cpu-type=vexiiriscv  --with-sdram --sdram-data-width=64 --bus-standard axi-lite --vexii-args="--allow-bypass-from=0 --with-mul --with-div --with-rva --with-btb --with-ras --with-gshare --fetch-l1-sets=64 --fetch-l1-ways=4 --lsu-l1-sets=64 --lsu-l1-ways=4 --lsu-l1-store-buffer-slots=2 --lsu-l1-store-buffer-ops=32 --lsu-l1-refill-count 2 --lsu-l1-writeback-count 2 --with-supervisor --with-user --performance-counters 0" --trace-fst --sdram-init /media/data2/proj/vexii/litex/buildroot/rv32ima/images/boot.json --trace-start 120000000000000 --trace-end 122000000000000 --trace
+
+litex_sim --cpu-type=vexiiriscv  --with-sdram --sdram-data-width=64  \
+--vexii-args=" \
+--allow-bypass-from=0 --debug-privileged --with-mul --with-div --div-ipc --with-rva --with-supervisor --performance-counters 0 \
+--regfile-async --xlen=64 --with-rvc --with-rvf --with-rvd \
+--fetch-l1 --fetch-l1-ways=4 --fetch-l1-mem-data-width-min=64 \
+--lsu-l1 --lsu-l1-ways=4  --lsu-l1-mem-data-width-min=64 --lsu-l1-refill-count 1 --lsu-l1-writeback-count 1 --with-lsu-bypass \
+--relaxed-branch"  --cpu-count=2 --with-jtag-tap  --with-jtagremote  --sdram-init boot.json
+
 --sdram-init /media/data2/proj/vexii/litex/buildroot/rv32ima/images/boot.json
 
 --decoders=2 --lanes=2 --with-dispatcher-buffer"
@@ -386,20 +396,36 @@ load_image /media/data2/proj/vexii/litex/buildroot/rv32ima/opensbi/build/platfor
 resume
 
 //Linux++ single issue 1.77
-python3 -m litex_boards.targets.digilent_nexys_video --cpu-type=vexiiriscv  --vexii-args="--allow-bypass-from=0 --debug-privileged --with-mul --with-div --div-ipc --with-rva --with-supervisor --performance-counters 0 --fetch-l1 --fetch-l1-ways=4 --lsu-l1 --lsu-l1-ways=4 --fetch-l1-mem-data-width-min=64 --lsu-l1-mem-data-width-min=64  --with-btb --with-ras --with-gshare --relaxed-branch --regfile-async --lsu-l1-store-buffer-slots=2 --lsu-l1-store-buffer-ops=32 --lsu-l1-refill-count 2 --lsu-l1-writeback-count 2 --with-lsu-bypass" --with-jtag-tap  --build --load
+python3 -m litex_boards.targets.digilent_nexys_video --cpu-type=vexiiriscv  --bus-standard axi-lite --vexii-args="--allow-bypass-from=0 --debug-privileged --with-mul --with-div --div-ipc --with-rva --with-supervisor --performance-counters 0 --fetch-l1 --fetch-l1-ways=4 --lsu-l1 --lsu-l1-ways=4 --fetch-l1-mem-data-width-min=64 --lsu-l1-mem-data-width-min=64  --with-btb --with-ras --with-gshare --relaxed-branch --regfile-async --lsu-l1-store-buffer-slots=2 --lsu-l1-store-buffer-ops=32 --lsu-l1-refill-count 2 --lsu-l1-writeback-count 2 --with-lsu-bypass" --with-jtag-tap  --build --load
 
 //Linux++ dual issue
-python3 -m litex_boards.targets.digilent_nexys_video --cpu-type=vexiiriscv  --vexii-args="--allow-bypass-from=0 --debug-privileged --with-mul --with-div --div-ipc --with-rva --with-supervisor --performance-counters 0 --fetch-l1 --fetch-l1-ways=4 --lsu-l1 --lsu-l1-ways=4 --fetch-l1-mem-data-width-min=64 --lsu-l1-mem-data-width-min=64  --with-btb --with-ras --with-gshare --relaxed-branch --regfile-async --lsu-l1-store-buffer-slots=2 --lsu-l1-store-buffer-ops=32 --lsu-l1-refill-count 2 --lsu-l1-writeback-count 2 --with-lsu-bypass --decoders=2 --lanes=2" --with-jtag-tap  --build --load
+python3 -m litex_boards.targets.digilent_nexys_video --cpu-type=vexiiriscv  --bus-standard axi-lite --vexii-args="--allow-bypass-from=0 --debug-privileged --with-mul --with-div --div-ipc --with-rva --with-supervisor --performance-counters 0 --fetch-l1 --fetch-l1-ways=4 --lsu-l1 --lsu-l1-ways=4 --fetch-l1-mem-data-width-min=64 --lsu-l1-mem-data-width-min=64  --with-btb --with-ras --with-gshare --relaxed-branch --regfile-async --lsu-l1-store-buffer-slots=2 --lsu-l1-store-buffer-ops=32 --lsu-l1-refill-count 2 --lsu-l1-writeback-count 2 --with-lsu-bypass --decoders=2 --lanes=2" --with-jtag-tap  --build --load
 
 //linux++ 64 bits fpu
-python3 -m litex_boards.targets.digilent_nexys_video --cpu-type=vexiiriscv  --with-jtag-tap --vexii-args=" \
+python3 -m litex_boards.targets.digilent_nexys_video --cpu-type=vexiiriscv  --with-jtag-tap  --bus-standard axi-lite --vexii-args=" \
 --allow-bypass-from=0 --debug-privileged --with-mul --with-div --div-ipc --with-rva --with-supervisor --performance-counters 0 \
---regfile-async --xlen=64 --with-rvc --with-rvf --with-rvd
+--regfile-async --xlen=64 --with-rvc --with-rvf --with-rvd \
 --fetch-l1 --fetch-l1-ways=4 --fetch-l1-mem-data-width-min=64 \
 --lsu-l1 --lsu-l1-ways=4  --lsu-l1-mem-data-width-min=64 --lsu-l1-store-buffer-ops=32 --lsu-l1-refill-count 2 --lsu-l1-writeback-count 2 --lsu-l1-store-buffer-slots=2  --with-lsu-bypass \
---with-btb --with-ras --with-gshare --relaxed-branch"  \
---cpu-count=2 --with-jtag-tap  --with-video-framebuffer --with-sdcard --with-ethernet --with-coherent-dma --l2-bytes=131072 \
---update-repo=no --build --load
+--with-btb --with-ras --with-gshare --relaxed-branch"  --cpu-count=2 --with-jtag-tap  --with-video-framebuffer --with-sdcard --with-ethernet --with-coherent-dma --l2-byte=131072 --update-repo=no  --sys-clk-freq 80000000 --build   --load  --sys-clk-freq 80000000
+
+
+python3 -m litex_boards.targets.digilent_nexys_video --cpu-type=vexiiriscv  --with-jtag-tap --bus-standard axi-lite  --vexii-args=" \
+--allow-bypass-from=0 --debug-privileged --with-mul --with-div --div-ipc --with-rva --with-supervisor --performance-counters 0 \
+--regfile-async --xlen=64 --with-rvc --with-rvf --with-rvd \
+--fetch-l1 --fetch-l1-ways=4 --fetch-l1-mem-data-width-min=64 \
+--lsu-l1 --lsu-l1-ways=4  --lsu-l1-mem-data-width-min=64 --lsu-l1-refill-count 1 --lsu-l1-writeback-count 1 --with-lsu-bypass \
+--relaxed-branch"  --cpu-count=2 --with-jtag-tap  --with-video-framebuffer --with-sdcard --with-ethernet --with-coherent-dma --l2-byte=0 --update-repo=no --build   --load  --sys-clk-freq 80000000
+
+
+python3 -m litex_boards.targets.digilent_nexys_video --cpu-type=vexiiriscv  --with-jtag-tap --bus-standard axi-lite --vexii-args=" \
+--allow-bypass-from=0 --debug-privileged --with-mul --with-div --div-ipc --with-rva --with-supervisor --performance-counters 0 \
+--regfile-async --xlen=32 --with-rvc --with-rvf --with-rvd \
+--fetch-l1 --fetch-l1-ways=4 --fetch-l1-mem-data-width-min=64 \
+--lsu-l1 --lsu-l1-ways=4  --lsu-l1-mem-data-width-min=64 --lsu-l1-refill-count 1 --lsu-l1-writeback-count 1 --with-lsu-bypass \
+--relaxed-branch"  --cpu-count=2 --with-jtag-tap  --with-video-framebuffer --with-sdcard --with-ethernet --with-coherent-dma --l2-byte=0 --update-repo=no --build   --load  --sys-clk-freq 100000000
+
+
 
 
 python3 -m litex_boards.targets.digilent_nexys_video --cpu-type=vexiiriscv  --vexii-args="--debug-privileged" --with-jtag-tap --build --load
@@ -414,6 +440,10 @@ python3 -m litex_boards.targets.digilent_nexys_video --cpu-type=vexiiriscv  --ve
 
 
 
+opensbi =>
+git clone https://github.com/Dolu1990/opensbi.git --branch uart-fix
+cd opensbi
+make PLATFORM_RISCV_XLEN=64 PLATFORM_RISCV_ABI=lp64d PLATFORM_RISCV_ISA=rv64gc CROSS_COMPILE=riscv-none-embed- PLATFORM=litex/vexriscv
 
 //linux ++ dual core
 make O=build/full  BR2_EXTERNAL=../config litex_vexriscv_full_defconfig
@@ -444,5 +474,71 @@ cat >> /etc/X11/xorg.conf << EOF
 >   Load "fbdevhw"
 > EndSection
 > EOF
+
+
+
+
+TODO debug :
+/media/data2/proj/vexii/litex/debian$ litex_sim --cpu-type=vexiiriscv  --with-sdram --sdram-data-width=64  --vexii-args=" \
+--allow-bypass-from=0 --debug-privileged --with-mul --with-div --div-ipc --with-rva --with-supervisor --performance-counters 0 \
+--regfile-async --xlen=64 --with-rvc --with-rvf --with-rvd \
+--fetch-l1 --fetch-l1-ways=4 --fetch-l1-mem-data-width-min=64 \
+--lsu-l1 --lsu-l1-ways=4  --lsu-l1-mem-data-width-min=64 --lsu-l1-refill-count 1 --lsu-l1-writeback-count 1 --with-lsu-bypass \
+--relaxed-branch"  --cpu-count=2 --with-jtag-tap  --with-jtagremote  --sdram-init boot.json --update-repo no
+
+[    0.074696] smp: Bringing up secondary CPUs ...
+[    0.089174] Oops - instruction access fault [#1]
+[    0.089305] CPU: 1 PID: 0 Comm: swapper/1 Not tainted 6.6.0-rc5+ #1
+[    0.089461] epc : __riscv_copy_words_unaligned+0xa/0x50
+[    0.089658]  ra : check_unaligned_access+0x84/0x216
+[    0.089814] epc : ffffffff80002706 ra : ffffffff80002d36 sp : ffffffc800093f80
+[    0.089936]  gp : ffffffff810cb770 tp : ffffffd8014fb5c0 t0 : 0000000000000003
+[    0.090048]  t1 : 0000000000000000 t2 : 000000000000000b s0 : ffffffc800093fe0
+[    0.090153]  s1 : 0000000000000001 a0 : ffffffd80154c001 a1 : ffffffd80154e003
+[    0.090266]  a2 : 0000000000001f80 a3 : ffffffd80154ff83 a4 : 0000000000001f80
+[    0.090375]  a5 : ffffffd7bf000000 a6 : ffffffd89df0e000 a7 : ffffffff81100430
+[    0.090496]  s2 : ffffffd80154c001 s3 : 0000000041001066 s4 : ffffffd81e97c7e0
+[    0.090611]  s5 : 0000000000000001 s6 : ffffffd80154e003 s7 : 0000000000000000
+[    0.090716]  s8 : 0000000000002000 s9 : 0000000000000000 s10: 0000000000000000
+[    0.090817]  s11: 0000000000000000 t3 : ffffffd81e97c7e0 t4 : 0000000000000002
+[    0.090923]  t5 : 0000000000000007 t6 : 0000000000000001
+[    0.090999] status: 0000000200000100 badaddr: ffffffff80002706 cause: 0000000000000001
+[    0.091118] [<ffffffff80002706>] __riscv_copy_words_unaligned+0xa/0x50
+[    0.091306] [<ffffffff80005db8>] smp_callin+0x48/0x64
+[    0.091673] Code: b57d c097 0072 80e7 47c0 7713 fc06 c729 86b3 00e5 (6198) 659c
+[    0.091775] ---[ end trace 0000000000000000 ]---
+[    0.091851] Kernel panic - not syncing: Fatal exception in interrupt
+[    0.091915] SMP: stopping secondary CPUs
+
+
+root@nexys:/home/miaou/readonly# /usr/games/numptyphysics
+[  675.176824] numptyphysics[614]: unhandled signal 11 code 0x2 at 0x0000002ab6814a00 in numptyphysics[2ab67e6000+4a000]
+[  675.185076] CPU: 1 PID: 614 Comm: numptyphysics Not tainted 6.1.0-rc2+ #11
+[  675.190719] epc : 0000002ab6814a00 ra : 0000002ab68149e4 sp : 0000003fe0d414e0
+[  675.196269]  gp : 0000002ab6834f10 tp : 0000003fa3a30780 t0 : 0000003fa43fa290
+[  675.202124]  t1 : 0000002ab67f309c t2 : 0000000000000023 s0 : 0000003fa208a014
+[  675.208050]  s1 : 0000003fe0d41598 a0 : 0000003fa20da030 a1 : 00000000000000ff
+[  675.213771]  a2 : ffffffffffffffff a3 : 0000000000004001 a4 : 0000000000000001
+[  675.219706]  a5 : 0000003fa208a024 a6 : 0000000000000000 a7 : 0000003fa20da030
+[  675.225435]  s2 : 0000002ab6914d98 s3 : 0000002ab68fbb70 s4 : 0000003fe0d41598
+[  675.231382]  s5 : 0000000000000001 s6 : 0000003fa43f8d80 s7 : 0000002ab68caae0
+[  675.237105]  s8 : 0000003fa43f9030 s9 : 0000003fa43f8d80 s10: 0000002ac2ad6a4c
+[  675.243062]  s11: 0000002ac2ad69c0 t3 : 0000003fa3ec77d2 t4 : 0000003fa3f740d0
+[  675.248770]  t5 : 0000000002ab68a4 t6 : 0000000000360ae0
+[  675.253094] status: 8000000200006020 badaddr: 0000002ab6814a00 cause: 0000000000000001
+
+
+xeyes
+
+
+
+debug fmax =>
+python3 -m litex_boards.targets.digilent_nexys_video --cpu-type=vexiiriscv  --with-jtag-tap  --bus-standard axi-lite --vexii-args=" \
+--allow-bypass-from=0 --debug-privileged --with-mul --with-div --div-ipc --with-rva --with-supervisor --performance-counters 0 \
+--regfile-async --xlen=64 --with-rvc --with-rvf --with-rvd --decoders=2 --lanes=2 --with-dispatcher-buffer \
+--fetch-l1 --fetch-l1-ways=4 --fetch-l1-mem-data-width-min=64 \
+--lsu-l1 --lsu-l1-ways=4  --lsu-l1-mem-data-width-min=64 --lsu-l1-store-buffer-ops=32 --lsu-l1-refill-count 2 --lsu-l1-writeback-count 2 --lsu-l1-store-buffer-slots=2  --with-lsu-bypass \
+--with-btb --with-ras --with-gshare --relaxed-branch"  --cpu-count=2 --with-jtag-tap  --with-video-framebuffer --with-sdcard --with-ethernet --with-coherent-dma --l2-byte=131072 --update-repo=no  --sys-clk-freq 80000000    --build  --sys-clk-freq 80000000
+
 
  */
