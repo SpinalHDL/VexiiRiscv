946f8f98350a61e52c20f3d6cf5b7ccf98ea8b27
Dolu1990 <charles.papon.90@gmail.com>
2024-10-28 14:34:25 +0100

    Add SocCtrl

---
+++ src/main/scala/vexiiriscv/soc/demo/SocCtrl.scala
@@ -0,0 +1,33 @@
+package vexiiriscv.soc.demo
+
+import spinal.core._
+import spinal.lib.ResetCtrlFiber
+import spinal.lib.cpu.riscv.debug.DebugModuleSocFiber
+
+
+class SocCtrlParam{
+  var withJtagTap = false
+  var withJtagInstruction = false
+  var systemFrequency = 100 MHz
+  def withDebug = withJtagTap ||  withJtagInstruction
+
+  def addOptions(parser: scopt.OptionParser[Unit]): Unit = {
+    import parser._
+    opt[Int]("system-frequency") action { (v, c) => systemFrequency = v Hz }
+    opt[Boolean]("jtag-tap") action { (v, c) => withJtagTap = v  }
+    opt[Boolean]("tag-instruction") action { (v, c) => withJtagInstruction = v}
+  }
+}
+
+class SocCtrl(p: SocCtrlParam) extends Area{
+  val systemClk = in Bool()
+  val systemClkCd = ClockDomain(systemClk, frequency = FixedFrequency(p.systemFrequency))
+
+  val asyncReset = in Bool()
+  val debug = systemClkCd(new ResetCtrlFiber().addAsyncReset(asyncReset, HIGH))
+  val system  = systemClkCd(new ResetCtrlFiber().addAsyncReset(debug))
+
+  val debugModule = p.withDebug generate debug.cd(new DebugModuleSocFiber(p.withJtagTap, p.withJtagInstruction){
+    system.addSyncRelaxedReset(dm.ndmreset, HIGH)
+  })
+}
