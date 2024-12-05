ad94547ec631667d61e0ca488daae823aa85986d
David Lanzendörfer <leviathan@libresilicon.com>
2024-12-05 17:34:33 +0000

    Adding a patch
    fesvr fails to compile without it

--- docker/setup_userspace.sh
+++ docker/setup_userspace.sh
@@ -4,11 +4,24 @@ cd /home/ubuntu
 
 git clone  --recursive https://github.com/SpinalHDL/VexiiRiscv.git
 
+pushd VexiiRiscv/ext/riscv-isa-sim/fesvr
+echo "Patching fesvr"
+git apply /home/ubuntu/fix_fesvr.patch
+popd
+
+mkdir VexiiRiscv/ext/riscv-isa-sim/build
+pushd VexiiRiscv/ext/riscv-isa-sim/build
+../configure --prefix=/opt/riscv --enable-commitlog  --without-boost --without-boost-asio --without-boost-regex
+make
+popd
+
+pushd VexiiRiscv/ext/rvls
+make
+popd
+
 pushd VexiiRiscv
 sbt update
 popd
-
-rm -rf VexiiRiscv
 
 git clone https://github.com/shioyadan/konata.git
 pushd konata
