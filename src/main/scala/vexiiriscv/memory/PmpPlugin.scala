9512e23858c2066842039bd13986c1fffd00a819
Dolu1990 <charles.papon.90@gmail.com>
2025-04-15 17:57:20 +0200

    Fix pmp lock not checking access in machine mode.

--- src/main/scala/vexiiriscv/memory/PmpPlugin.scala
+++ src/main/scala/vexiiriscv/memory/PmpPlugin.scala
@@ -186,12 +186,12 @@ class PmpPlugin(val p : PmpParam) extends FiberPlugin with PmpService{
     portsLock.await()
 
     val isMachine = priv.isMachine(0)
-    val checkInstruction = !isMachine
-    val checkData = !isMachine || priv.logic.harts(0).m.status.mprv && priv.logic.harts(0).m.status.mpp =/= 3
+    val instructionShouldHit = !isMachine
+    val dataShouldHit = !isMachine || priv.logic.harts(0).m.status.mprv && priv.logic.harts(0).m.status.mpp =/= 3
 
     val ports = for(ps <- portSpecs) yield new Composite(ps.rsp, "logic", false){
       import ps._
-      val portCheckData = checkData || ps.forceCheck(permStage)
+      val dataShouldHitPort = dataShouldHit || ps.forceCheck(permStage)
       val torCmpAddress = torCmpStage(ps.physicalAddress) >> granularityWidth
       val TOR_SMALLER = new Array[Payload[Bool]](p.pmpSize)
       val onEntries = for((e, id) <- entries.zipWithIndex) yield new Area{
@@ -206,19 +206,22 @@ class PmpPlugin(val p : PmpParam) extends FiberPlugin with PmpService{
         }
         val HIT_ANY = { import hitsStage._ ; insert(p.withNapot.mux(e.isNapot && napot.HIT, False) || p.withTor.mux(e.isTor && tor.HIT, False)) }
 
-        val normalRwx = (!ps.read(permStage) || e.cfg.read || !checkInstruction) &&
-                        (((!ps.write(permStage) || e.cfg.write) && (!ps.execute(permStage) || e.cfg.execute)) || !portCheckData)
+        val instructionCheck = e.cfg.locked || instructionShouldHit
+        val dataCheck = e.cfg.locked || dataShouldHitPort
+        val normalRwx = (!ps.read(permStage) || e.cfg.read || !instructionCheck) &&
+                        (((!ps.write(permStage) || e.cfg.write) && (!ps.execute(permStage) || e.cfg.execute)) || !dataCheck)
         val PERM_OK = permStage.insert(normalRwx)
       }
-      val NEED_HIT = permStage.insert(checkInstruction && ps.execute(permStage) || portCheckData && (ps.read(permStage) || ps.write(permStage)))
+      val NEED_HIT = permStage.insert(instructionShouldHit && ps.execute(permStage) || dataShouldHitPort && (ps.read(permStage) || ps.write(permStage)))
 
       val onCtrl = (p.pmpSize > 0) generate new Area{
         import ps.rspStage._
         val hits = Cat(onEntries.map(e => ps.rspStage(e.HIT_ANY)))
         val oh = OHMasking.firstV2(hits)
         val reader = onEntries.reader(oh)
+        val entriesReader = entries.reader(oh)
 
-        rsp.ACCESS_FAULT := NEED_HIT && !(reader(e => ps.rspStage(e.PERM_OK)))
+        rsp.ACCESS_FAULT := (NEED_HIT || entriesReader(e => e.isLocked)) && !(reader(e => ps.rspStage(e.PERM_OK)))
       }
       if(p.pmpSize == 0) ps.rspStage(rsp.ACCESS_FAULT) := False
     }
