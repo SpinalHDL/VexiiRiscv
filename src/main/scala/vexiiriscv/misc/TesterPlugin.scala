d66c98daa4a061aea4d1259199c8776cd6fda088
Dolu1990 <charles.papon.90@gmail.com>
2025-09-12 17:53:36 +0200

    Add mmu refill interface in TesterPlugin

--- src/main/scala/vexiiriscv/misc/TesterPlugin.scala
+++ src/main/scala/vexiiriscv/misc/TesterPlugin.scala
@@ -4,11 +4,12 @@ import spinal.core._
 import spinal.lib.misc.plugin.FiberPlugin
 import vexiiriscv.execute.CsrService
 import vexiiriscv.execute.lsu.LsuPlugin
+import vexiiriscv.memory.{AddressTranslationService, MmuPlugin}
 
 import scala.collection.mutable.ArrayBuffer
 
 class TesterPlugin extends FiberPlugin{
-  val logic = during build{
+  val logic = during build new Area{
     val l = ArrayBuffer(
       host[TrapPlugin].api.harts(0).holdPrivChange,
       host[CsrService].bus.decode.fence
@@ -20,4 +21,33 @@ class TesterPlugin extends FiberPlugin{
       l.foreach(_:= True)
     }
   }
+
+  class Lsfr(width : Int = 32, mask : BigInt = BigInt(0xC0000401l), seed : BigInt = BigInt(0xC0000401l)) extends Area{
+    val value = Reg(Bits(width bits)) init(seed)
+    val enable = Bool()
+    when(enable){
+      value := ((value & mask).xorR ## value) >> 1
+    }
+  }
+
+  val ptw = during setup (host.get[MmuPlugin].nonEmpty generate new Area{
+    val ats = host[AddressTranslationService]
+    val earlyLock = retains(ats.portsLock)
+
+    awaitBuild()
+
+    val ptw = ats.newRefillPort()
+    earlyLock.release()
+
+    val random = new Lsfr(seed = 0x12345678)
+    random.enable := !ptw.cmd.isStall
+
+    val address = new Lsfr()
+    address.enable := !ptw.cmd.isStall
+
+    ptw.cmd.valid := random.value(3, 9 bits) === 0
+    ptw.cmd.address := address.value.asSInt.resize(widthOf(ptw.cmd.address)).asUInt
+    ptw.cmd.storageId := 0
+    ptw.cmd.storageEnable := False
+  })
 }
